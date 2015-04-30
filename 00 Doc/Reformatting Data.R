f3d931b3df06cf151c4b51660aaf70ba4f10e0ea
Justin Owens <justinowens@utexas.edu>
2015-04-30 17:14:25 -0500

    fixing reformatting data.r

--- 00 Doc/Reformatting Data.R
+++ 00 Doc/Reformatting Data.R
@@ -1,14 +1,22 @@
-setwd("C:/Users/asif/Documents/GitHub/DataVisualization/DV_FinalProject")
+setwd("~/Mine/UT/GitRepositories/DataVisualization/RWorkshop/15 Reformatting Data")
+# file_path <- "Sample - Superstore - English (Extract).csv"
+# measures <- c("Customer_ID", "Discount", "Number_of_Records", "Order_ID", "Order_Quantity", "Product_Base_Margin", "Profit", "Sales", "Shipping_Cost", "Unit_Price" )
 
-file_path <- "ProviderCharges.csv"
-measures <- c("provider_id", "provider_zip_code", "total_discharges", "average_covered_charges", "average_total_payments","serialid")
+# file_path <- "Sample - Superstore Subset (Excel).csv"
+# measures <- c("Row_ID", "Discount", "Unit_Price", "Shipping_Cost", "Customer_ID", "Product_Base_Margin", "Postal_Code", "Profit", "Quantity_ordered_new", "Sales", "Order_ID")
+
+#file_path <- "Advanced Crosstabs - Superstore Subset (Excel).csv"
+#measures <- c("Customer_ID", "Order_ID", "Postal_Code", "Row_ID", "Number_of_Records", "Order_Quantity", "Product_Base_Margin", "Profit", "Sales", "Shipping_Cost", "Unit_Price", "Discount", "Gross_Profit_Ratio")
+
+file_path <- "April16AdvTblCalc.csv"
+measures <- c("Customer_ID", "Order_ID", "Postal_Code", "Row_ID", "Number_of_Records", "Order_Quantity", "Product_Base_Margin", "Profit", "Sales", "Shipping_Cost", "Unit_Price", "Discount", "Gross_Profit_Ratio" )
 
 df <- read.csv(file_path, stringsAsFactors = FALSE)
 
 # Replace "." (i.e., period) with "_" in the column names.
 names(df) <- gsub("\\.+", "_", names(df))
 
-str(df)
+# str(df) # Uncomment this to get column types to use for getting the list of measures.
 
 # Get rid of special characters in each column.
 # Google ASCII Table to understand the following:
@@ -27,13 +35,20 @@ for(d in dimensions) {
 }
 
 library(lubridate)
+# Fix date columns, this needs to be done by hand because | needs to be correct.
+#                                                        \_/
+df$Order_Date <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Order_Date), tz="UTC")))
+df$Ship_Date  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Ship_Date),  tz="UTC")))
+
+# The following is an example of dealing with special cases like making state abbreviations be all upper case.
+# df["State"] <- data.frame(lapply(df["State"], toupper))
 
 # Get rid of all characters in measures except for numbers, the - sign, and period.
 for(m in measures) {
-  df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.,0-9]",replacement= ""))
+  df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
 }
 
-write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE)
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
 
 tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
 sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
@@ -46,4 +61,3 @@ for(m in measures) {
 }
 sql <- paste(sql, ");")
 cat(sql)
-
